3d3cc7e31e8a736b0e65e4dfff19e1ebb180830e
Cecilia Gunawan <your_email@example.com>
2026-09-02 22:47:39 +0700

    fix : Merge Conflict

--- src/types/route.ts
+++ src/types/route.ts
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import { ComponentType } from "react";
-
-export type HimtiPermission =
-  | "manage_urls"
-  | "manage_users"
-  | "manage_roles"
-  | "manage_permissions"
-  | "manage_events"
-  | "manage_batch"
-  | "manage_certificates"
-  | "create_events"
-  | "view_dashboard";
-
-export interface Route {
-  key: string;
-  title: string;
-  description: string;
-  component: ComponentType;
-  path: string;
-  isEnabled: boolean;
-  isProtected: boolean;
-=======
 import { ComponentType } from "react";
 
 export type HimtiPermission =
@@ -30,6 +7,7 @@ export type HimtiPermission =
   | "manage_permissions"
   | "manage_events"
   | "manage_elections"
+  | "manage_certificates"
   | "view_election_results"
   | "manage_batch"
   | "create_events"
@@ -43,7 +21,6 @@ export interface Route {
   path: string;
   isEnabled: boolean;
   isProtected: boolean;
->>>>>>> origin/dev
   requiredPermission?: HimtiPermission;
   allowedRoles?: string[];
   group?: string;
